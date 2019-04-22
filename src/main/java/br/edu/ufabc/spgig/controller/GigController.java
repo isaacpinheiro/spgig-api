2c82eb1c226cff40ddfdd2ac05db6725dc8885c9
Isaac Pinheiro <isaacpnhr@gmail.com>
2019-04-22 16:00:11 -0300

    Implementando a camada controller.

--- src/main/java/br/edu/ufabc/spgig/controller/GigController.java
+++ src/main/java/br/edu/ufabc/spgig/controller/GigController.java
@@ -9,6 +9,46 @@ package br.edu.ufabc.spgig.controller;
  *
  * @author isaac
  */
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import br.edu.ufabc.spgig.model.Gig;
+import br.edu.ufabc.spgig.repository.GigRepository;
+
+@RestController
 public class GigController {
+    
+    @Autowired
+    private GigRepository repository;
+    
+    @RequestMapping(value="/api/gig/{id}", method=RequestMethod.GET, produces="application/json")
+    public @ResponseBody Gig listOne(@PathVariable("id") String id) {
+        Gig obj = repository.findOne(Long.parseLong(id));
+        return obj;
+    }
+    
+    @RequestMapping(value="/api/gig", method=RequestMethod.GET, produces="application/json")
+    public @ResponseBody Iterable<Gig> findAll() {
+        return repository.findAll();
+    }
+    
+    @RequestMapping(value="/api/gig", method=RequestMethod.POST, consumes="application/json")
+    public @ResponseBody String insert(@RequestBody Gig obj) {
+        repository.save(obj);
+        return "{\"msg\": \"success\"}";
+    }
+    
+    @RequestMapping(value="/api/gig/{id}", method=RequestMethod.DELETE)
+    public @ResponseBody String delete(@PathVariable("id") String id) {
+        Gig obj = repository.findOne(Long.parseLong(id));
+        repository.delete(obj);
+        return "{\"msg\": \"success\"}";
+    }
     
 }
