a051b8fc52e20b0a3ef35ae6ec3b1ceafc241108
Isaac Pinheiro <isaacpnhr@gmail.com>
2019-04-22 00:38:56 -0300

    Implementando a camada repository.

---
+++ src/main/java/br/edu/ufabc/spgig/repository/OrganizadorRepository.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.ufabc.spgig.repository;
+
+/**
+ *
+ * @author isaac
+ */
+
+import org.springframework.data.repository.CrudRepository;
+import br.edu.ufabc.spgig.model.Organizador;
+
+public interface OrganizadorRepository extends CrudRepository<Organizador, Long> {
+    
+}
